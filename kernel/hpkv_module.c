5163cf976e12638aec21d44721d4df2771db5561
Mehran Toosi <mehran.toosi@visma.com>
2024-09-24 17:06:07 +0200

    fixing compile errors

--- kernel/hpkv_module.c
+++ kernel/hpkv_module.c
@@ -24,6 +24,7 @@
 #include <linux/rculist.h>
 #include <linux/delay.h>
 #include <linux/kthread.h>
+#include <linux/hashtable.h>
 
 #define DEVICE_NAME "hpkv"
 #define MAX_KEY_SIZE 256
@@ -892,7 +893,7 @@ static int extend_device(loff_t new_size)
     hpkv_log(HPKV_LOG_INFO, "Attempting to extend device to %lld bytes\n", new_size);
 
     // Reopen the block device in read-only mode
-    bdev_ro = blkdev_get_by_path(mount_path, FMODE_READ, THIS_MODULE);
+    bdev_ro = blkdev_get_by_path(mount_path, FMODE_READ, NULL, THIS_MODULE);
     if (IS_ERR(bdev_ro)) {
         hpkv_log(HPKV_LOG_ERR, "Failed to reopen block device in read-only mode\n");
         return PTR_ERR(bdev_ro);
@@ -2017,7 +2018,7 @@ static int __init hpkv_init(void)
     }
 
     hpkv_log(HPKV_LOG_INFO, "Attempting to open block device: %s\n", mount_path);
-    bdev = blkdev_get_by_path(mount_path, FMODE_READ | FMODE_WRITE, THIS_MODULE);
+    bdev = blkdev_get_by_path(mount_path, FMODE_READ | FMODE_WRITE, NULL, THIS_MODULE);
     if (IS_ERR(bdev)) {
         hpkv_log(HPKV_LOG_ALERT, "Failed to open block device, error %ld\n", PTR_ERR(bdev));
         ret = PTR_ERR(bdev);
