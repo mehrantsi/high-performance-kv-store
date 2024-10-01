98e2c0a89d737c7c2fc64332b1f127a3a833efe1
Mehran Toosi <mehran.toosi@visma.com>
2024-10-01 23:29:30 +0200

    fixed multi arch module naming

--- start.sh
+++ start.sh
@@ -3,6 +3,21 @@
 set -e
 set -x
 
+# Determine the system architecture
+ARCH=$(uname -m)
+case ${ARCH} in
+    x86_64)
+        MODULE_ARCH="x86_64"
+        ;;
+    aarch64)
+        MODULE_ARCH="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: ${ARCH}"
+        exit 1
+        ;;
+esac
+
 # Create a file to use as a block device if it doesn't exist
 if [ ! -f /app/hpkv_disk.img ]; then
     dd if=/dev/zero of=/app/hpkv_disk.img bs=1M count=1024
@@ -26,7 +41,7 @@ losetup $LOOP_DEVICE /app/hpkv_disk.img
 
 # Load the kernel module
 cd /app/kernel
-insmod hpkv_module.ko mount_path=$LOOP_DEVICE
+insmod hpkv_module_${MODULE_ARCH}.ko mount_path=$LOOP_DEVICE
 
 # Start the Node.js server
 cd /app/api
@@ -38,7 +53,7 @@ cleanup() {
     # Stop the Node.js server
     pkill -f "node server.js"
     # Unload the kernel module
-    rmmod hpkv_module
+    rmmod hpkv_module_${MODULE_ARCH}
     # Remove any leftover loop devices
     losetup -D
 }
