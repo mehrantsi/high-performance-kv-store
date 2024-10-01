292f97cb9d9dc7efeef7188b111f3ce5038bc259
Mehran Toosi <mehran.toosi@visma.com>
2024-10-01 22:55:15 +0200

    add multi arch support to docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -3,20 +3,35 @@
 # Set the version variable
 VERSION="v1.2-docker"
 
+# Determine the system architecture
+ARCH=$(uname -m)
+case ${ARCH} in
+    x86_64)
+        DOCKER_ARCH="amd64"
+        ;;
+    aarch64)
+        DOCKER_ARCH="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: ${ARCH}"
+        exit 1
+        ;;
+esac
+
 # Check if the image already exists
 if docker image inspect "hpkv-image:${VERSION}" &> /dev/null; then
     echo "Docker image hpkv-image:${VERSION} already exists. Skipping download."
 else
     # Download the Docker image
     echo "Downloading Docker image..."
-    wget "https://github.com/mehrantsi/high-performance-kv-store/releases/download/${VERSION}/hpkv-image-arm64.tar"
+    wget "https://github.com/mehrantsi/high-performance-kv-store/releases/download/${VERSION}/hpkv-image-arm64-amd64.tar.gz"
 
     # Load the Docker image
     echo "Loading Docker image..."
-    docker load < hpkv-image-arm64.tar
+    docker load < hpkv-image-arm64-amd64.tar.gz
 
-    # Clean up the downloaded tar file
-    rm hpkv-image-arm64.tar
+    # Clean up the downloaded tar.gz file
+    rm hpkv-image-arm64-amd64.tar.gz
 fi
 
 # Run the Docker container
@@ -26,7 +41,8 @@ docker run --rm \
   --privileged \
   --device /dev/loop-control:/dev/loop-control \
   -p 4242:80 \
-  "hpkv-image-arm64:${VERSION}"
+  --platform linux/${DOCKER_ARCH} \
+  "hpkv-image:${VERSION}"
 
 # The container will be automatically removed when it exits due to the --rm flag
 
